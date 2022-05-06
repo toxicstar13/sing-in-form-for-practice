ff13649d6ce5e9041c43002b3d8220e5ad834a6d
toxicstar13 <11030047@fhsh.tp.edu.tw>
2022-05-06 11:58:34 +0800

    基本版型完成

--- src/main.js
+++ src/main.js
@@ -4,4 +4,21 @@ $(document).ready(function () {
   $("#sing-in-form").submit(function (e) {
     e.preventDefault();
   });
+
+  //TODO: 讓帳號密碼輸入時，版型維持
+  $("#account").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#addr-hint-text").addclass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#addr-hint-text").removeclass("-translate-y-5 text-xs text-sky-500");
+    }
+  });
+
+  $("#password").blur(function (e) {
+    if ($(e.target).val() !== "") {
+      $("#addr-hint-text").addclass("-translate-y-5 text-xs text-sky-500");
+    } else {
+      $("#addr-hint-text").removeclass("-translate-y-5 text-xs text-sky-500");
+    }
+  });
 });
